8b246676e1ea5a1e43c9c9bc1ebd68817c27314c
zhangminfeng <317209267@qq.com>
2020-09-26 15:19:29 +0800

    '对讲'

--- handles/handleReceivedOfferICE.js
+++ handles/handleReceivedOfferICE.js
@@ -8,6 +8,8 @@ module.exports.receivedOfferICE = async function(data,documentEl){
     //添加远端发送过来的offer_ice信息,
     //通过这种方式就将answer端的icecandidate添加上了
     if(answerPc){
+        console.log(answerPc);
+        console.log("-------");
         console.log(data);
         await answerPc.addIceCandidate(new RTCIceCandidate(data));
     }
