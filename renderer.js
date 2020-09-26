36c08aeec7caa9d52d15d2c7bc24c0420cd17738
zhangminfeng <317209267@qq.com>
2020-09-26 15:33:18 +0800

    '对讲'

--- renderer.js
+++ renderer.js
@@ -245,6 +245,8 @@ $(document).ready(function(){
         //8.提示关闭弹框
         global.getData(global.LAYER_OBJ).msg("连接已断开...",{time:2000});
 
+
+        console.log("********");
         console.log(global.getData(global.KEY_LOCAL_MEDIA_STREAM));
         console.log(global.getData(global.KEY_REMOTE_MEDIA_STREAM));
         console.log(vueObj);
