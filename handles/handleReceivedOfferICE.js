80c0001204968cc49c6f4ec935a0a93d47beab86
zhangminfeng <317209267@qq.com>
2020-09-28 13:50:29 +0800

    '对讲'

--- handles/handleReceivedOfferICE.js
+++ handles/handleReceivedOfferICE.js
@@ -1,12 +1,14 @@
 let global = require("../global/globalFile");
 
 module.exports.receivedOfferICE = async function(data,documentEl){
+    console.log("进来了");
 
     //获取answer链接对象
     let answerPc =  global.getData(global.KEY_ANSWER_PEER_CONNECTION);
 
     //添加远端发送过来的offer_ice信息,
     //通过这种方式就将answer端的icecandidate添加上了
+    console.log(answerPc);
     if(answerPc){
         console.log(data);
         await answerPc.addIceCandidate(new RTCIceCandidate(data));
