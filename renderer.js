677ee063ccef47291f764dec4ffea32c9a924113
zhangminfeng <317209267@qq.com>
2020-09-29 10:45:28 +0800

    '改版对讲'

--- renderer.js
+++ renderer.js
@@ -196,15 +196,18 @@ $(document).ready(function(){
 
     //answerPc端接收到了offerPc端的ice消息
     $(documentEl).on("offerPc_ice",async function(e,data){
-        console.log("answerPc端本地处理ice信息");
-        console.log(data);
         //获取answer链接对象
-        let answerPc = await new Promise((resolve) => {
-            let answerPc = global.getData(global.KEY_ANSWER_PEER_CONNECTION);
+        /*let answerPc = await new Promise((resolve) => {
+
             if(answerPc){
                 resolve(answerPc);
             }
-        });
+        });*/
+
+        let answerPc = await global.getData(global.KEY_ANSWER_PEER_CONNECTION);
+
+        console.log("answerPc端本地处理ice信息");
+        console.log(data);
 
         //在answerPc端处理ice信息
         handleReceivedOfferICE.receivedOfferICE(data,documentEl,answerPc);
